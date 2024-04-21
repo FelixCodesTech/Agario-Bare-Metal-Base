f314db2abd1e3d7fb019dd6e55ee3bf83d506a0b
Felix <66774630+FelixCodesTech@users.noreply.github.com>
2024-04-21 17:01:08 +0200

    first commit

---
+++ pastyThing.js
@@ -0,0 +1,11 @@
+// Paste the contents of play.html between the backticks
+const playHtmlContent = `
+
+
+
+`;
+
+// Replace the entire website with the contents of play.html
+document.open();
+document.write(playHtmlContent);
+document.close();
