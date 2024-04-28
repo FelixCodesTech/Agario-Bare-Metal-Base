d18f5ccd28a942956a8da09ba5276c240be2dd22
Felix <66774630+FelixCodesTech@users.noreply.github.com>
2024-04-28 15:01:36 +0200

    improve enemy and food targeting logic

--- jsScripts/fromLM/autoplay.js
+++ jsScripts/fromLM/autoplay.js
@@ -64,18 +64,22 @@ function calcTarget() {
 
     var distcounterflag = 0;
     let shortestDistanceEnemy = 10000;
+    let shortestDistanceFood = 10000;
 
     Object.keys(window.legendmod.cells).forEach(node => { //function to define and act to cells
         distcounterflag++
         PlayerCell = window.legendmod.cells[node];
         let distancePlayerCell = calcDist(PlayerCell.x, PlayerCell.y);
-        if (PlayerCell.nick != window.legendmod.playerNick) { // if not me
-            // if not virus
-            if (PlayerCell.isVirus == false) {
-                if (calcDist(PlayerCell.x, PlayerCell.y) < shortestDistanceEnemy) {
-                    shortestDistanceEnemy = calcDist(PlayerCell.x, PlayerCell.y);
-                    target.x = 2 * window.legendmod.playerX - PlayerCell.x;
-                    target.y = 2 * window.legendmod.playerY - PlayerCell.y;
+        if (PlayerCell.nick != window.legendmod.playerNick && PlayerCell.isVirus == false) { // if not me
+            if (calcDist(PlayerCell.x, PlayerCell.y) + PlayerCell.size*10 < shortestDistanceEnemy && PlayerCell.size > window.legendmod.playerSize) {
+                shortestDistanceEnemy = calcDist(PlayerCell.x, PlayerCell.y);
+                target.x = 2 * window.legendmod.playerX - PlayerCell.x;
+                target.y = 2 * window.legendmod.playerY - PlayerCell.y;
+            } else if (PlayerCell.size < window.legendmod.playerSize && calcDist(PlayerCell.x, PlayerCell.y) < shortestDistanceFood) {
+                shortestDistanceFood = calcDist(PlayerCell.x, PlayerCell.y);
+                if (shortestDistanceFood < shortestDistanceEnemy) {
+                    target.x = PlayerCell.x;
+                    target.y = PlayerCell.y;
                 }
             }
         }
