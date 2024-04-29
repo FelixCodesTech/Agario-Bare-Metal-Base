66771dd8efafbdab489622c8b284024ae74c89e2
Felix <66774630+FelixCodesTech@users.noreply.github.com>
2024-04-29 18:53:12 +0200

    Fix

--- jsScripts/fromLM/autoplay.js
+++ jsScripts/fromLM/autoplay.js
@@ -72,7 +72,7 @@ function calcTarget() {
         let distancePlayerCell = calcDist(PlayerCell.x, PlayerCell.y);
         if (PlayerCell.nick != window.legendmod.playerNick && PlayerCell.isVirus == false) { // if not me
             if (calcDist(PlayerCell.x, PlayerCell.y) - PlayerCell.size/10 < shortestDistanceEnemy && PlayerCell.size > window.legendmod.playerSize * 1.15) {
-                shortestDistanceEnemy = calcDist(PlayerCell.x, PlayerCell.y);
+                shortestDistanceEnemy = calcDist(PlayerCell.x, PlayerCell.y) - PlayerCell.size/10;
                 target.x = 2 * window.legendmod.playerX - PlayerCell.x;
                 target.y = 2 * window.legendmod.playerY - PlayerCell.y;
             } else if (PlayerCell.size * 1.15 < window.legendmod.playerSize && calcDist(PlayerCell.x, PlayerCell.y) < shortestDistanceFood) {
